fe601cc882aaec75d5d7ef225a1cad4916901bc4
John Carr <john.carr@unrouted.co.uk>
2014-11-09 19:27:30 +0000

    Successfully connect to syncthing

--- pysyncthing.py
+++ pysyncthing.py
@@ -157,19 +157,42 @@ packet_stream = Struct("packet_stream",
 )
 
 
-from OpenSSL import SSL
+from OpenSSL import SSL, crypto
 import sys, os, select, socket
 
-def verify_cb(conn, cert, errnum, depth, ok):
-    # This obviously has to be updated
-    print 'Got certificate: %s' % cert.get_subject()
-    return ok
+if not os.path.exists("client.key"):
+    print "Generating private key"
+    private_key = crypto.PKey()
+    private_key.generate_key(crypto.TYPE_RSA, 3096)
+
+    print "Generating cert"
+    cert = crypto.X509()
+    subj = cert.get_subject()
+    subj.C = "US"
+    subj.ST = "Minnesota"
+    subj.L = "Minnetonka"
+    subj.O = "my company"
+    subj.OU = "my organization"
+    subj.CN = "syncthing"
+    cert.set_serial_number(1000)
+    cert.gmtime_adj_notBefore(0)
+    cert.gmtime_adj_notAfter(10*365*24*60*60)
+    cert.set_issuer(subj)
+    cert.set_pubkey(private_key)
+    cert.sign(private_key, 'sha1')
+
+    with open("client.crt", "w") as fp:
+        fp.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
+
+    with open("client.key", "w") as fp:
+        fp.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, private_key))
+
 
 # Initialize context
 ctx = SSL.Context(SSL.TLSv1_2_METHOD)
-ctx.set_verify(SSL.VERIFY_PEER, verify_cb) # Demand a certificate
-#ctx.use_privatekey_file (os.path.join(os.cwd, 'client.pkey'))
-#ctx.use_certificate_file(os.path.join(os.cwd, 'client.cert'))
+#ctx.set_verify(SSL.VERIFY_PEER, verify_cb) # Demand a certificate
+ctx.use_privatekey_file('client.key')
+ctx.use_certificate_file('client.crt')
 #ctx.load_verify_locations(os.path.join(os.cwd, 'CA.cert'))
 
 # Set up client
