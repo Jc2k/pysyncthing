644ec6099e0f26bfdea44a0c1e8668aedebefeef
John Carr <john.carr@unrouted.co.uk>
2014-11-30 13:29:16 +0000

    Add LGPL headers

--- pysyncthing/announce/local.py
+++ pysyncthing/announce/local.py
@@ -1,3 +1,22 @@
+# -*- Mode: Python; py-indent-offset: 4 -*-
+# pysyncthing - GNOME implementation of the syncthing engine
+# Copyright (C) 2014 John Carr
+#
+#   pysyncthing/announce/local.py: Local discovery of other syncthing devices
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, see <http://www.gnu.org/licenses/>.
+
 import socket
 import logging
 
