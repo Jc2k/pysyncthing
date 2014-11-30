346fdff018a2ea95cb71c303962cf65f8b1df49a
John Carr <john.carr@unrouted.co.uk>
2014-11-30 12:39:15 +0000

    Use pgi if its available

--- pysyncthing/__main__.py
+++ pysyncthing/__main__.py
@@ -1,3 +1,9 @@
+try:
+    import pgi
+    pgi.install_as_gi()
+except ImportError:
+    pass
+
 from .engine import Engine
 
 Engine("example").run()
--- pysyncthing/repository.py
+++
@@ -1,4 +0,0 @@
-try:
-    from pgi.repository import GLib, Gio
-except:
-    from gi.repository import GLib, Gio
